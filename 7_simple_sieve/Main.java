147a60d3b19033c5ec14ec6a98b60818a94d061a
scar027 <shantanupai@hotmail.com>
2024-12-08 21:45:43 +0530

    fix: simpler implementation
    Signed-off-by: scar027 <shantanupai@hotmail.com>

--- 7_simple_sieve/Main.java
+++ 7_simple_sieve/Main.java
@@ -1,31 +1,36 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main{
-  public static void main(String[] args) {
+public class Main {
+  public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
-    System.out.print("Enter a number: ");
+
+    System.out.println("Enter a number");
     int num = sc.nextInt();
 
-    // Create a boolean array and initialize all to true
-    boolean[] isPrime = new boolean[num + 1];
-    for (int i = 2; i <= num; i++) {
-      isPrime[i] = true;
-    }
+    boolean[] bool = new boolean[num];
 
-    // Mark non-prime numbers
-    for (int i = 2; i * i <= num; i++) {
-      if (isPrime[i]) {
-        for (int j = i * i; j <= num; j += i) {
-          isPrime[j] = false;
+    // Initially mark all numbers as prime (true)
+    Arrays.fill(bool, true);
+
+    // 0 and 1 are not prime numbers
+    bool[0] = bool[1] = false;
+
+    // Implementing Sieve of Eratosthenes
+    for (int i = 2; i < Math.sqrt(num); i++) {
+      if (bool[i]) {
+        // Mark all multiples of i as non-prime
+        for (int j = i * i; j < num; j += i) {
+          bool[j] = false;
         }
       }
     }
 
+    System.out.println("List of prime numbers up to the given number are:");
+
     // Print all prime numbers
-    System.out.println("Prime numbers up to " + num + ":");
-    for (int i = 2; i <= num; i++) {
-      if (isPrime[i]) {
-        System.out.print(i + " ");
+    for (int i = 2; i < bool.length; i++) {
+      if (bool[i]) {
+        System.out.println(i);
       }
     }
     sc.close();
